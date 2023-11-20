7f0808a3c4cf34228d0624a2893f9c352477e104
Yuditia <ardyudit@gmail.com>
2023-11-20 21:06:27 +0700

    Third Commit

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5501;
 
+app.use(express.static(path.join(__dirname, 'assets/css')));
+app.use(express.static(path.join(__dirname, 'assets/bootstrap/css')));
+app.use(express.static(path.join(__dirname, 'assets/bootstrap/js')));
+app.use(express.static(path.join(__dirname, 'assets/font')));
+app.use(express.static(path.join(__dirname, 'assets/img')));
+app.use(express.static(path.join(__dirname, 'assets/js')));
+app.use(express.static(path.join(__dirname, 'assets/svg')));
 app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/my-project.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'my-project.html'));
 });
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,7 +2,6 @@ function submitData() {
     let name = document.getElementById("input-name").value;
     let email = document.getElementById("input-email").value;
     let phone = document.getElementById("input-number").value;
-    let subject = document.getElementById("input-subject").value;
     let massage = document.getElementById("input-massage").value;
   
     if (name == "") {
@@ -11,8 +10,6 @@ function submitData() {
       return alert("Email must be filled in!");
     } else if (phone == "") {
       return alert("Phone must be charged!");
-    } else if (subject == "") {
-      return alert("subject must be filled in!");
     } else if (massage == "") {
       return alert("Don't forget to fill in the message!");
     }
@@ -20,13 +17,12 @@ function submitData() {
     let emailReceiver = "ardyudit@gmail.com";
   
     let a = document.createElement("a");
-    a.href = `mailto:${emailReceiver}?subject=${subject}&body=Hello, my name is ${name}, message: ${massage}. If there is a problem please call this number ${phone}, Thanks.`;
+    a.href = `mailto:${emailReceiver}?body=Hello, my name is ${name}, message: ${massage}. If there is a problem please call this number ${phone}, Thanks.`;
     a.click();
   
     console.log(name);
     console.log(email);
     console.log(phone);
-    console.log(subject);
     console.log(massage);
   }
   
